09336f0d0e10f12653b9e429ff898b401a776374
harika penukonda <penukondaharika@gmail.com>
2020-09-15 11:27:58 -0500

    Using state

--- react_demo/src/App.js
+++ react_demo/src/App.js
@@ -3,14 +3,22 @@ import './App.css';
 import Person from './Person/Person';
 
 class App extends Component {
+  state = {
+    persons: [
+      { name: 'Harika', age:25 },
+      { name: 'Sindhu', age:23 },
+      { name: 'Anjali', age:21 }
+
+    ]
+  }
   render() {
     return (
       <div className="App">
         <h1>Hi, I'm a React App</h1>
         <p>This is really working!</p>
-        <Person name="Harika" age="25"/>
-        <Person name="Sindhu" age="23">My Hobbies: Watching Movies</Person>
-        <Person name="Anjali" age="21"/>
+        <Person name={this.state.persons[0].name} age={this.state.persons[0].age}/>
+        <Person name={this.state.persons[1].name} age={this.state.persons[1].age}>My Hobbies: Watching Movies</Person>
+        <Person name={this.state.persons[2].name} age={this.state.persons[2].age}/>
 
 
       </div>
