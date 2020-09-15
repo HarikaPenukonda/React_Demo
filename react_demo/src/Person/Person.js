45ffa40d8656d43af00efc804a242ff40f558522
harika penukonda <penukondaharika@gmail.com>
2020-09-15 10:45:39 -0500

    outputting dynamic content

--- react_demo/src/Person/Person.js
+++ react_demo/src/Person/Person.js
@@ -1,7 +1,7 @@
 import React from 'react'
 
 const person = () =>{
-    return <p>I am a Person! and I am Math.floor(Math.random()*20) years old!</p>
+    return <p>I am a Person! and I am {Math.floor(Math.random()*20)} years old!</p>
 };
 
 export default person;
